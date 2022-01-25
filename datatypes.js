07f58d91611c8a3430b054296dfa79cd0d6a7e86
Thomas Buckley <tombuckley91@gmail.com>
2022-01-25 21:03:44 +0000

    Introduce datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+var carla = "not pink tonight!"; // string
+var numberOfTimesCarlaHasNotWornPink = 1; // number
+var hasBrew = true; // boolean
+
+var bav; // undefined
+
+var komal = null; // null
+
+var students = [
+  {
+    name: "Najma",
+    hidden: true,
+  },
+  {
+    name: "Hassan",
+    hidden: false,
+  },
+  {
+    name: "Humayra",
+    hidden: true,
+  },
+]; // 3
